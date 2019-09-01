e3741e3d7416560607e59d805db1e9e24e0492fd
pportelette <pierre.portelette@free.fr>
2019-09-01 15:19:48 +0800

    set default value for consult and graph sections

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Search;
+use TS\AssetsBundle\Entity\Site;
 
 class AppController extends Controller
 {
@@ -13,7 +15,34 @@ class AppController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $em = $this->getDoctrine()->getManager();
+        $siteRepository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('TSAssetsBundle:Site')
+        ;
+
+        $search = new Search();
+        $startDate = new \DateTime('2018-01-01');
+        $endDate = new \DateTime('2018-01-31');
+
+        $site = $siteRepository->find(1);
+
+        $search->setStartDate($startDate);
+        $search->setEndDate($endDate);
+        $search->setSite($site);
+
+        $session=$request->getSession();
+        $session->set('search', $search);
+
+        $rowsRepository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('TSDataManagerBundle:ImportDataRow')
+        ;
+        $rows = $rowsRepository->getSelectedData($search->getStartDate(), $search->getEndDate(), $site);
+        $session->set('rows', $rows);
+
         return $this->render('@App/App/homepage.html.twig');
     }
 }
